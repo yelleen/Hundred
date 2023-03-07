2b2d5766d3d9be2bea801ecd1993e47b2450b626
yelleen <yumizu@naver.com>
2023-03-07 21:29:17 +0900

    add Number.cs

---
+++ Cards/Number.cs
@@ -0,0 +1,24 @@
+﻿public class Number : Card
+{
+    public Number(int num)
+    {
+        number = num;
+        skill = Skill.NORMAL;
+        state = "Normal";
+        isCalced = true;
+    }
+    public override int GetNumber()
+    {
+        return number;
+    }
+    public override void SkillEffect(Card c)
+    {
+        Calc.SetCount(c.GetNumber());
+    }
+    public override void UseCard()
+    {
+        Calc.BufferShift(this);
+        Calc.State = state;
+        Calc.Previous.SkillEffect(Calc.Now);
+    }
+}
